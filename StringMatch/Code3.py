bd5fb29b87d1ba3d832942a2ab46282242da08ed
1183830041 <69196644+1183830041@users.noreply.github.com>
2020-09-23 18:48:10 +0800

    Add files via upload

---
+++ StringMatch/Code3.py
@@ -0,0 +1,8 @@
+#3.实现 strStr()
+def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0:
+            return 0 
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
